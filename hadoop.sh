1288d6dd9be079fcb980ad4664033cdaabefc5b8
jiguangxia <xia95@126.com>
2016-11-30 11:23:03 +0800

    Create hadoop.sh

---
+++ hadoop.sh
@@ -0,0 +1,18 @@
+# help
+hadoop
+
+# version
+hadoop version
+
+# hdfs
+hadoop fs
+hadoop fs -ls [dir_name]
+hadoop fs -cat file_name
+hadoop fs -mkdir dir_name
+hadoop fs -put local_file hdfs_file
+hadoop fs -get hdfs_file [local_file]
+hadoop fs -rm file_name
+hadoop fs -rmdir dir_name
+
+# mapreduce
+hadoop jar
