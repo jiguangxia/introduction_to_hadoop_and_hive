ef84443a295577d60e72c28f5e76868894ca5578
jiguangxia <xia95@126.com>
2016-12-01 11:26:42 +0800

    Update hadoop.sh

--- hadoop.sh
+++ hadoop.sh
@@ -4,7 +4,9 @@ hadoop
 # version
 hadoop version
 
+#
 # hdfs
+#
 hadoop fs
 hadoop fs -ls [dir_name]
 hadoop fs -cat file_name
@@ -14,5 +16,17 @@ hadoop fs -get hdfs_file [local_file]
 hadoop fs -rm file_name
 hadoop fs -rmdir dir_name
 
+#
 # mapreduce
-hadoop jar
+#
+
+hadoop jar app.jar [class_name] input_path output_path
+
+# input_path is a file
+hadoop jar word_count.jar WordCount mr/lecture0/file1 mr/lecture0/result1
+
+# input_path is a directory
+hadoop jar word_count.jar WordCount mr/lecture0/input mr/lecture0/output
+
+# if your .jar has specified a MAIN CLASS, you have to remove [class_name]
+hadoop jar word_pair_count.jar mr/lecture0/input mr/lecture0/output
